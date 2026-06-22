348016b74b5733cb9b45c87f9e60785cd270f63f
999next0 <yejinliao720929@gmail.com>
2026-06-22 18:13:10 +0800

    代码迁移部分skill和模板，生成的各产出结果，运行到迁移程序检查步骤

---
+++ tests/migration_src/REQ_001/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/All_About_The_Code/VS Code repository/work_surrogatemodels/afsim-analysis-skill-project/tests/migration_src/REQ_001
+# Build directory: E:/All_About_The_Code/VS Code repository/work_surrogatemodels/afsim-analysis-skill-project/tests/migration_src/REQ_001/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(REQ_001_test_demo "E:/All_About_The_Code/VS Code repository/work_surrogatemodels/afsim-analysis-skill-project/tests/migration_src/REQ_001/build/test_demo.exe")
+set_tests_properties(REQ_001_test_demo PROPERTIES  _BACKTRACE_TRIPLES "E:/All_About_The_Code/VS Code repository/work_surrogatemodels/afsim-analysis-skill-project/tests/migration_src/REQ_001/CMakeLists.txt;88;add_test;E:/All_About_The_Code/VS Code repository/work_surrogatemodels/afsim-analysis-skill-project/tests/migration_src/REQ_001/CMakeLists.txt;0;")
